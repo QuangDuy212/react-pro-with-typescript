e00cd59c3cf8fc71af26456c05e748e156602cbb
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-05-18 21:51:31 +0700

    video22

---
+++ src/screens/Portfolio.tsx
@@ -0,0 +1,44 @@
+import LeftPart from "../components/LeftPart";
+import PreLoader from "../components/PreLoader";
+import RightPart from "../components/RightPart";
+import MobileMenu from "../components/mobile/menu";
+
+const Portfolio = () => {
+    return (
+        <>
+            <div className="arlo_tm_wrapper_all">
+
+                <div id="arlo_tm_popup_blog">
+                    <div className="container">
+                        <div className="inner_popup scrollable"></div>
+                    </div>
+                    <span className="close"><a href="#"></a></span>
+                </div>
+
+                {/* {/* <!-- PRELOADER --> */}
+                <PreLoader />
+                {/* <!-- /PRELOADER --> */}
+
+                {/* <!-- MOBILE MENU --> */}
+                <MobileMenu />
+                {/* <!-- /MOBILE MENU --> */}
+
+                {/* <!-- CONTENT --> */}
+                <div className="arlo_tm_content">
+
+                    {/* <!-- LEFTPART --> */}
+                    <LeftPart />
+                    {/* <!-- /LEFTPART --> */}
+
+                    {/* <!-- RIGHTPART --> */}
+                    <RightPart />
+                    {/* <!-- /RIGHTPART --> */}
+
+                    <a className="arlo_tm_totop" href="#"></a>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default Portfolio;
