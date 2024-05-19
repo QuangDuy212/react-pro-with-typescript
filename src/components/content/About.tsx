efd3948859d0361ce6ebd54a491666268de94722
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-05-19 18:39:09 +0700

    video29

--- src/components/content/About.tsx
+++ src/components/content/About.tsx
@@ -1,6 +1,7 @@
 import duy1 from '@/assets/img/about/duy1.jpg';
 import about from '@/assets/img/about/550x640.jpg';
 import cvduy from '@/assets/duy.pdf';
+import { TypeAnimation } from 'react-type-animation';
 
 const About = () => {
     return (
@@ -10,7 +11,7 @@ const About = () => {
                     <div className="container">
                         <div className="arlo_tm_title_holder">
                             <h3>About Me</h3>
-                            <span>Main informations about me</span>
+                            <span>Thông tin chính </span>
                         </div>
                         <div className="arlo_tm_about_wrap">
                             <div className="author_wrap">
@@ -30,42 +31,56 @@ const About = () => {
                                 </div>
                                 <div className="rightbox">
                                     <div className="arlo_tm_mini_title_holder">
-                                        <h4>I'm Alan Michaelis and <span className="arlo_tm_animation_text_word"></span></h4>
+                                        <h4><p>Hi,  I'm a
+                                            <> </>
+                                            <TypeAnimation
+                                                sequence={[
+                                                    // Same substring at the start will only be typed out once, initially
+                                                    'Freelancer',
+                                                    2000, // wait 1s before replacing "Mice" with "Hamsters"
+                                                    'UI/UX Designer',
+                                                    2000,
+                                                    'Web Developer',
+                                                    2000,
+                                                ]}
+                                                wrapper="span"
+                                                speed={50}
+                                                repeat={Infinity}
+                                            // style={{ fontSize: '2em', display: 'inline-block' }}
+                                            />
+                                        </p></h4>
                                     </div>
                                     <div className="definition">
-                                        <p>Hi! My name is <strong>Alan Michaelis</strong>. I am a Web Developer, and I'm very passionate and dedicated to my work. With 20 years experience as a professional Web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration to concept and execution, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.</p>
+                                        <p>Xin chào mình là <strong>Duy</strong>. Vốn là lập trình viên website, cơ mà mình có sở thích là No code.</p>
+                                        &nbsp;
+                                        <p>Mình đã học nhiều thứ về frontend nhưng vẫn chưa đủ. Mình luôn theo đuổi đam mê code web của mình.</p>
+                                        &nbsp;
+                                        <p>Hiện tại mình là sinh viên năm 2. </p>
                                     </div>
                                     <div className="about_short_contact_wrap">
                                         <ul>
                                             <li>
-                                                <span><label>Birthday:</label> 01.07.1990</span>
+                                                <span><label>Ngày sinh:</label> 10.01.2004</span>
                                             </li>
                                             <li>
-                                                <span><label>Age:</label> 28</span>
+                                                <span><label>Giới tính:</label> Name</span>
                                             </li>
                                             <li>
-                                                <span><label>City:</label> New York, USA</span>
+                                                <span><label>Trường:</label> Đại học Công nghiệp Hà Nội</span>
                                             </li>
                                             <li>
-                                                <span><label>Interests:</label> Soccer, UFC</span>
+                                                <span><label>Chuyên ngành:</label> Công nghệ thông tin</span>
                                             </li>
                                             <li>
-                                                <span><label>Study:</label> Chicago University</span>
+                                                <span><label>Facebook:</label>
+                                                    <a href="https://www.facebook.com/profile.php?id=100079731466553">Duy Nguyễn</a>
+                                                </span>
                                             </li>
                                             <li>
-                                                <span><label>Degree:</label> Master</span>
+                                                <span><label>Tốt nghiệp:</label> Giỏi</span>
                                             </li>
                                             <li>
-                                                <span><label>Website:</label> <a href="#">www.mywebsite.com</a></span>
-                                            </li>
-                                            <li>
-                                                <span><label>Mail:</label> <a href="mailto:example@gmail.com">mymail&#64;gmail.com</a></span>
-                                            </li>
-                                            <li>
-                                                <span><label>Phone:</label> <a href="#">+77 022 177 05 05</a></span>
-                                            </li>
-                                            <li>
-                                                <span><label>Twitter:</label> <a href="#">&#64;myusername</a></span>
+                                                <span><label>Mail:</label> <a href="mailto:duy2k4ml1234@gmail.com">duy2k4ml1234&#64;gmail.com</a></span>
                                             </li>
                                         </ul>
                                     </div>
